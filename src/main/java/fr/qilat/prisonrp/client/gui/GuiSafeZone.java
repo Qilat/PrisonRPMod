3d2d873afd2bcc6ee4868c6abf7cb554d42d3084
Qilat <theophile.vieux@gmail.com>
2017-12-06 21:42:07 +0100

    update gitignore + safezone gui

--- src/main/java/fr/qilat/prisonrp/client/gui/GuiSafeZone.java
+++ src/main/java/fr/qilat/prisonrp/client/gui/GuiSafeZone.java
@@ -1,7 +1,28 @@
 package fr.qilat.prisonrp.client.gui;
 
+import fr.qilat.prisonrp.PrisonRPCore;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.util.ResourceLocation;
+
 /**
  * Created by Qilat on 06/12/2017 for forge-1.10.2-12.18.3.2511-mdk.
  */
-public class GuiSafeZone {
+public class GuiSafeZone extends GuiScreen {
+    public static final String ROOT_DIRECTORY = "textures/gui/safezone/";
+    public static final ResourceLocation DEFAULT_BACKGROUND = new ResourceLocation(PrisonRPCore.MODID, ROOT_DIRECTORY + "defaultbackground.png");
+
+    public GuiSafeZone() {
+        super();
+    }
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+
+    }
+
+    @Override
+    public void initGui() {
+        super.initGui();
+    }
 }
